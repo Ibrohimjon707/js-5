67c225fb50c46cef8ef1504d15a55cc2c9e9ab58
Ibrohimjon707 <ibrohimjonathamjonov38@gmail.com>
2025-01-20 14:42:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+alert("Asslomu Alaykum xush kelibsiz");
+let a = +prompt("Birinchi raqamni kiriting");
+let b = +prompt("Ikkinchi sonni kiriting");
+let d = -prompt("Xoxlagan bittam amalingizni kiriting *-+/");
+if ((d = "*")) {
+  alert(a * b);
+} else if ((d = "-")) {
+  alert(a - b);
+} else if ((d = "+")) {
+  alert(a + b);
+} else if ((d = "/")) {
+  alert(a / b);
+}
